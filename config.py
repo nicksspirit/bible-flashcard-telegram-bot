082848e4580d3b849c1bf9a390b13bbba8c0d32d
Nick Muoh <nickspirit3+github@gmail.com>
2023-02-20 18:50:48 +0000

    Added file and line number to logformat

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ SHEETS_API_KEY = os.getenv("GOOGLE_SHEETS_API_KEY")
 GOOGLE_SHEETS_ID = os.getenv("GOOGLE_SHEETS_ID")
 
 
-LOG_MSG_FORMAT = "[%(asctime)s] %(levelname)s - %(name)s - %(message)s"
+LOG_MSG_FORMAT = "[%(asctime)s] %(levelname)s - %(name)s - %(filename)s:%(lineno)s - %(message)s"
 LOG_DATE_FORMAT = "%m/%d/%Y %I:%M:%S %p"
 LOG_DIR = Path("logs")
 LOG_DIR.mkdir(exist_ok=True)
